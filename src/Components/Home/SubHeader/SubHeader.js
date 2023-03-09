6f05191240e2b09dd98bfc091c607b34b3017414
IshKevin <carterk279@gmail.com>
2023-03-09 11:58:50 +0200

    make subheader

---
+++ src/Components/Home/SubHeader/SubHeader.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SubHeader = () => {
+  return (
+    <section>
+        <div className='subheader-sect1'>
+        <img src="Madein.PNG" alt='our log'/>
+        </div>
+        <div className='subheader-sect2'>
+        <p className='subheader-p'>Made in Rwanda</p>
+        </div>
+        <div className='subheader-sect3'>
+        <p className='subheader-p'>Handcrafted</p>
+        </div>
+    </section>
+  )
+}
+
+export default SubHeader
