fa6f2296e4b7868fc9f2cb69576e347ca34f09cc
IshKevin <carterk279@gmail.com>
2023-03-09 12:39:17 +0200

    fix subheader

--- src/Components/Home/SubHeader/SubHeader.js
+++ src/Components/Home/SubHeader/SubHeader.js
@@ -7,7 +7,7 @@ const SubHeader = () => {
         <img src="Madein.PNG" alt='our log'/>
         </div>
         <div className='subheader-sect2'>
-        <p className='subheader-p'>Made in Rwanda</p>
+    
         </div>
         <div className='subheader-sect3'>
         <p className='subheader-p'>Handcrafted</p>
