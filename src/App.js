1c2246b9e57eeba964bd23eb420b19c1b338a8e1
IshKevin <carterk279@gmail.com>
2023-03-09 10:29:52 +0200

    initial commit from kevin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return 
-  
+import React from 'react'
+import Login from './Login'
+
+const App = () => {
+  return (
+    <div><Login/></div>
+  )
 }
 
-export default App;
+export default App
