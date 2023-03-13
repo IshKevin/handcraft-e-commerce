31b51973c474e0c23c51f4fd535b8e130a945244
kunda pauline <kundapauline2002@gmail.com>
2023-03-13 15:46:19 +0200

    after app route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './App.css';
-import { BrowserRouter, Routes, Route }  from 'react-router-dom';
+import { BrowserRouter, Routes, Route,Outlet }  from 'react-router-dom';
  import Home  from './Pages/Home'
  import VendorDashboard  from './Pages/VendorDashboard/VendorDashboard';
 import AdminDashboard from './Pages/AdminDashboard';
+import VendorSidebar from './Components/Vendor/VendorSidebar/VendorSidebar';
+import Product from './Pages/VendorDashboard/Product';
 // import AdminContent from './Pages/AdminContent';
 
 
@@ -14,9 +16,22 @@ const App = () => {
     <>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="VenderDashboard" element={<VendorDashboard/>}/>
-      <Route path="AdminDashboard" element={<AdminDashboard/>}/> 
+      <Route   path  ="/"   element={<h1>Client</h1>} >
+      <Route index element={<Home />}/>
+      </Route>
+      
+      <Route path="VendorDashboard"  element={
+    <div className='dash'>
+    <VendorSidebar/>
+    <Outlet />
+      </div>
+    }>
+      <Route index element={<VendorDashboard/>}/>
+      <Route path="product" element={<Product/>}/>
+
+
+
+      </Route>
       {/* <Route path="dashBoard" element={<DashBoard posts={posts}/>}/>
       <Route path="/:blogId" element={<SinglePost posts={posts}/>}/>
       <Route path="Manage" element={<Manage posts={posts}/>}/>
