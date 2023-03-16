2bb4813d2e5fad40310bf7ee57d184bed3791d6a
IshKevin <carterk279@gmail.com>
2023-03-16 22:40:37 +0200

    fix footer

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -3,57 +3,55 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <footer class="site-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12 col-md-6">
+    <footer className="site-footer">
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12 col-md-6">
           <h6>About</h6>
-          <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i>
-           is an initiative  to help the upcoming programmers with the code. 
-           Scanfcode focuses on providing the most efficient code or snippets
-            as the code wants to be simple. We will help programmers build up 
-            concepts in different programming languages that include C, C++,
+          <p className="text-justify">Madein<i>SHOP THAT MAKE IT EASY TO GET MADE IN RWANDA</i><br/>
+           is an initiative  to help the upcoming programmers with the code. <br/>
+           Scanfcode focuses on providing the most efficient code or snippets<br/>
+            as the code wants to be simple. We will help programmers build up <br/>
+            concepts in different programming languages that include C, C++,<br/>
              Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
         </div>
 
-        <div class="col-xs-6 col-md-3">
-          <h6>Categories</h6>
-          <ul class="footer-links">
-            <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
-            <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
-            <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
-            <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
-            <li><a href="http://scanfcode.com/category/android/">Android</a></li>
-            <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+        <div className="col-xs-6 col-md-3">
+          <h1>Categories</h1>
+          <ul className="footer-links">
+            <li><a href="#">Women</a></li>
+            <li><a href="#">Men</a></li>
+            <li><a href="#">Shoe</a></li>
+            <li><a href="#">Watchs</a></li>
+            
           </ul>
         </div>
 
-        <div class="col-xs-6 col-md-3">
-          <h6>Quick Links</h6>
-          <ul class="footer-links">
-            <li><a href="http://scanfcode.com/about/">About Us</a></li>
-            <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
-            <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
-            <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
-            <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+        <div className="col-xs-6 col-md-3">
+          <h1>Quick Links</h1>
+          <ul className="footer-links">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Sitemap</a></li>
           </ul>
         </div>
       </div>
     </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 col-sm-6 col-xs-12">
-          <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 col-sm-6 col-xs-12">
+          <p className="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
        <a href="#">Scanfcode</a>.
           </p>
         </div>
 
-        <div class="col-md-4 col-sm-6 col-xs-12">
-          <ul class="social-icons">
-            <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-            <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-            <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-            <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+        <div className="col-md-4 col-sm-6 col-xs-12">
+          <ul className="social-icons">
+            <li><a className="facebook" href="#"><i className="fa fa-facebook"></i></a></li>
+            <li><a className="twitter" href="#"><i className="fa fa-twitter"></i></a></li>
+            <li><a className="dribbble" href="#"><i className="fa fa-dribbble"></i></a></li>
+            <li><a className="linkedin" href="#"><i className="fa fa-linkedin"></i></a></li>   
           </ul>
         </div>
       </div>
