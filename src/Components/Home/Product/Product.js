a64f2b989d68223c29edce72691135358f117236
IshKevin <carterk279@gmail.com>
2023-03-17 10:22:16 +0200

    design product page

--- src/Components/Home/Product/Product.js
+++ src/Components/Home/Product/Product.js
@@ -1,8 +1,54 @@
 import React from 'react'
+import ProductDisplay from '../../OneProduct/ProductDisplay'
+import './Product.css'
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <>
+    <h1>Clothing & Apparel</h1>
+    <div className='product-disp'>
+    <div className='product-disp-card'>left appear card</div>
+    <div className='product-disp-list'>
+    <ProductDisplay
+        imageUrl="Home/sport.jpg"
+        name="Product 1"
+        rating={5}
+        price="$19.99"
+      />
+      <ProductDisplay
+        imageUrl="Home/sport.jpg"
+        name="Product 1"
+        rating={5}
+        price="$19.99"
+      />
+      <ProductDisplay
+        imageUrl="Home/sport.jpg"
+        name="Product 1"
+        rating={5}
+        price="$19.99"
+      />
+      <ProductDisplay
+        imageUrl="Home/sport.jpg"
+        name="Product 1"
+        rating={5}
+        price="$19.99"
+      />
+      <ProductDisplay
+        imageUrl="Home/sport.jpg"
+        name="Product 1"
+        rating={5}
+        price="$19.99"
+      />
+      <ProductDisplay
+        imageUrl="Home/sport.jpg"
+        name="Product 1"
+        rating={5}
+        price="$19.99"
+      />
+      </div>
+    </div>
+
+    </>
   )
 }
 
