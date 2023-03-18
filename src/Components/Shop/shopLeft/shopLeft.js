3465d91a0c992b412f58a09447340eec7e450646
IshKevin <carterk279@gmail.com>
2023-03-19 00:32:05 +0200

    g

--- src/Components/Shop/shopLeft/shopLeft.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const shopLeft = () => {
-  return (
-    <div>shopLeft</div>
-  )
-}
-
-export default shopLeft
--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../../Components/Home/Header/Header'
 import SubHeader from '../../Components/Home/SubHeader/SubHeader'
 import Footer from '../../Components/Home/Footer/Footer'
-import ShopLeft from '../../Components/Shop/ShopLeft/ShopLeft'
+
 import Shopdata from '../../Components/Shop/Shopdata/Shopdata'
 
 
@@ -12,7 +12,6 @@ const Shop = () => {
       <Header/>
       <SubHeader/>
       <div className="shop-container">
-        <ShopLeft/>
         <Shopdata/>
       </div>
       <Footer/>
