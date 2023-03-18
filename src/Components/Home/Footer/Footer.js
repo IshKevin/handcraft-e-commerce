8647d956420b5f00efb09f665311d52f714ead8f
IshKevin <carterk279@gmail.com>
2023-03-18 22:24:18 +0200

    add LOG file

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -1,14 +1,60 @@
 import React from 'react'
 import './Footer.css'
+import {AiFillFacebook, AiFillTwitterCircle, AiFillInstagram} from 'react-icons/ai'
 
 const Footer = () => {
   return (
     <footer className="site-footer">
     <div className='footer-top_sec'>
-      top section
+      <div className='footer-top_sec1 '>
+       <h1>CATEGOLIES</h1>
+       <ul>
+        <li>Women</li>
+        <li>Men</li>
+        <li>Shoes</li>
+        <li>Watchs</li>
+       </ul>
+   </div>
+   <div className='footer-top_sec1 '>
+       <h1>HELP</h1>
+       <ul>
+        <li>Track order</li>
+        <li>Returns</li>
+        <li>Shipping</li>
+        <li>FAQs</li>
+       </ul>
+   </div>
+   <div className='footer-top_sec1 '>
+       <h1>GET IN TOUCH</h1>
+       <p>
+        Any questions? Let us know <br/> 
+        in store at 8th floor, 379St, Kigali,<br/> 
+        or call us on (+1) 96 716 6879
+       </p>
+       <div classNmae='footer-icons'>
+        <AiFillFacebook/>
+        <AiFillTwitterCircle/>
+        <AiFillInstagram/>
+       </div>
+   </div>
+   <div className='footer-top_sec1 '>
+       <h1>NEWS</h1>
+        <p>
+          <input type='text' placeholder='email@ex.co'/>
+          <button>Subscribe</button>
+        </p>
+   </div>
     </div>
     <div className='footer-down_sec'>
-      down
+      <div className="footer-flag">
+       <div><img src='Flag/Flag1.jpg' alt='mtn'/></div>
+        <div><img src='Flag/Flag2.jpg' alt='mtn'/></div>
+        <div><img src='Flag/Flag3.png' alt='mtn'/></div>
+        <div><img src='Flag/Flag4.png' alt='mtn'/></div> 
+      </div>
+      <div className='footer-closing'>
+        copyright &copy; 2023 All right reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://klab.com" target="_blank">Klab</a>  
+      </div>
     </div>
 </footer>
   )
