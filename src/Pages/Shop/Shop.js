9520339c4154a0acc3cdd973f3f44b5240f7d1a3
IshKevin <carterk279@gmail.com>
2023-03-19 00:03:12 +0200

    make shange on shop page

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -2,17 +2,18 @@ import React from 'react'
 import Header from '../../Components/Home/Header/Header'
 import SubHeader from '../../Components/Home/SubHeader/SubHeader'
 import Footer from '../../Components/Home/Footer/Footer'
-import ProductDisplay from '../../Components/OneProduct/ProductDisplay'
-
+import ShopLeft from '../../Components/Shop/ShopLeft/ShopLeft'
+import Shopdata from '../../Components/Shop/Shopdata/Shopdata'
 
 const Shop = () => {
   return (
     <div>
       <Header/>
       <SubHeader/>
-      <h1>
-        <ProductDisplay/>
-      </h1>
+      <div className="shop-container">
+        <ShopLeft/>
+        <Shopdata/>
+      </div>
       <Footer/>
       </div>
   )
