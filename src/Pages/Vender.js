7e75b30f11999e215fbaba8ebef2abc3b8ea2ef7
IshKevin <carterk279@gmail.com>
2023-03-19 23:28:34 +0200

    start vander

--- src/Pages/Vender.js
+++ src/Pages/Vender.js
@@ -3,12 +3,14 @@ import Footer from '../Components/Home/Footer/Footer'
 import Header from '../Components/Home/Header/Header'
 import SubHeader from '../Components/Home/SubHeader/SubHeader'
 import VenderBody from '../Components/Vender/VenderBody'
+import CategoliesNav from '../Components/CategoliesNav/CategoliesNav'
 
 const Vender = () => {
   return (
     <div>
       <Header/>
       <SubHeader />
+      <CategoliesNav/>
       <VenderBody/>
       <Footer/>
     </div>
