6a11489a8211378f76c91c128a3ea6cff94b9f1f
IshKevin <carterk279@gmail.com>
2023-03-19 22:45:44 +0200

    add new shop product

--- src/Components/Shop/Shopdata/Shopdata.js
+++ src/Components/Shop/Shopdata/Shopdata.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Shopdata.css'
+
 
 const Shopdata = () => {
   return (
-    <div>
-        <h1>shop product</h1>
+    <div className='shopdata'>
+      <div>
+        
+      </div>
+      <div>p2</div>
+      <div>p3</div>
+      <div>p4</div>
+      <div>p5</div>
+      <div>p6</div>
+      <div>p7</div>
+      <div>p8</div>
+      <div>p9</div>  
     </div>
   )
 }
