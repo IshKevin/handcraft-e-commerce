553722f279a325256c0b93748da34da7cb7a4cf0
IshKevin <carterk279@gmail.com>
2023-03-24 15:27:03 +0200

    wishlist page

--- src/Pages/Wishlist/Wishlist.js
+++ src/Pages/Wishlist/Wishlist.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from '../../Components/Home/Header/Header'
+import SubHeader from '../../Components/Home/SubHeader/SubHeader'
+import Footer from '../../Components/Home/Footer/Footer'
 
 const Wishlist = () => {
   return (
     <div>
-        welcome to wish list
+      <Header/>
+      <SubHeader/>
+     Wishlist page
+      <Footer/>
     </div>
   )
 }
