286bc0087e60825173bb1786a8c9ba4391918ace
IshKevin <carterk279@gmail.com>
2023-03-25 20:35:18 +0200

    make new wishlist style

--- src/Pages/Wishlist/Wishlist.js
+++ src/Pages/Wishlist/Wishlist.js
@@ -2,13 +2,18 @@ import React from 'react'
 import Header from '../../Components/Home/Header/Header'
 import SubHeader from '../../Components/Home/SubHeader/SubHeader'
 import Footer from '../../Components/Home/Footer/Footer'
-
+import CategoliesNav from '../../Components/CategoliesNav/CategoliesNav'
+import './Wishlist.css'
 const Wishlist = () => {
   return (
     <div>
       <Header/>
       <SubHeader/>
-     Wishlist page
+      
+      <div className='Wishlist-style'>
+      <div className='wishlist-cat1'><CategoliesNav/></div>
+      <div className='wishlist-cat2'>wishlist component</div>
+      </div>
       <Footer/>
     </div>
   )
