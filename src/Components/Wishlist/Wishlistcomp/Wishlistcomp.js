db7373dc69df740f8b78c79a80f8b9394964c5ec
IshKevin <carterk279@gmail.com>
2023-03-26 21:27:40 +0200

    start workin on cart page

--- src/Components/Wishlist/Wishlistcomp/Wishlistcomp.js
+++ src/Components/Wishlist/Wishlistcomp/Wishlistcomp.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Wishlistcomp = () => {
   return (
-    <div>Wishlistcomp</div>
+    <div>
+      <input type='checkbox'/>
+      <img src='/public/Flag/Flag1.jpg'/>
+      <p>product name</p>
+    </div>
   )
 }
 
